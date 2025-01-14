5d02c7dd0c37599def5ef97224024af63362b299
gijong07 <gijong07@gmail.com>
2025-01-14 14:31:40 +0900

    텍스쳐 퀄리티 업
    텍스쳐 퀄리티 업

--- Build/Metamate_Git_WebGL.loader.js
+++ Build/Metamate_Git_WebGL.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 101654077,
+    wasmFileSize: 101654126,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
